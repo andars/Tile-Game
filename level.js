522f916045855527691001239f5fc532a6824870
andars <afoote97@gmail.com>
2013-05-05 17:12:01 -0600

    Update level.js

--- level.js
+++ level.js
@@ -32,7 +32,7 @@ Level.prototype = {
 		}
 	},
 	render: function(){
-		console.log(this.miny);
+		//console.log(this.miny);
 		for (var i = Math.floor(this.minx/50); i < MAP_WIDTH; i++){
 		
 			if (i * TILE_DIM + TILE_DIM <= this.minx || i*TILE_DIM>(this.minx+CANVAS_WIDTH))
