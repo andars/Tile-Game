440c9d64e5ea4c7ced2ad1d16053ef79c3c82f5b
andars <afoote97@gmail.com>
2013-05-06 12:29:17 -0600

    Update level.js

--- level.js
+++ level.js
@@ -44,7 +44,7 @@ Level.prototype = {
 				if (j * TILE_DIM+TILE_DIM < this.miny || j * TILE_DIM > this.miny + CANVAS_HEIGHT)
 					continue;
 				
-				this.getTile(i,j).draw(this.ctx, this.img, (i * TILE_DIM) - this.minx, (j * TILE_DIM) - this.miny);
+				this.getTile(i,j).draw(this.ctx, this.img, (i * TILE_DIM) - this.minx, (j * TILE_DIM) - this.miny, this, i, j);
 				
 			}
 		}
