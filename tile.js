c4e169422f2134e37b45a819d5897bf1a3fc8aef
andars <afoote97@gmail.com>
2013-05-07 17:04:49 -0600

    Update tile.js

--- tile.js
+++ tile.js
@@ -17,13 +17,13 @@ Tile.prototype = {
 			var e = level.getTile(tx+1, ty) === this;
 			if (!n||!s||!w||!e){
 				//northwest
-				ctx.drawImage(img, this.drawx - (w?0:25), this.drawy + (n?0:(-TILE_DIM/2)), TILE_DIM/2,TILE_DIM/2,x,y,TILE_DIM/2,TILE_DIM/2);
+				ctx.drawImage(img, this.drawx - (!w?0:25), this.drawy + (n?0:(-TILE_DIM/2)), TILE_DIM/2,TILE_DIM/2,x,y,TILE_DIM/2,TILE_DIM/2);
 				//southwest
-				ctx.drawImage(img, this.drawx + (w?0:(-TILE_DIM/2)), this.drawy + (s?0:(TILE_DIM)), TILE_DIM/2, TILE_DIM/2, x, y,TILE_DIM/2,TILE_DIM/2);
+				ctx.drawImage(img, this.drawx + (!w?0:(-TILE_DIM/2)), this.drawy + (s?0:(TILE_DIM)), TILE_DIM/2, TILE_DIM/2, x, y,TILE_DIM/2,TILE_DIM/2);
 				//northeast
-				ctx.drawImage(img, this.drawx + (e?0:(TILE_DIM)), this.drawy + (n?0:(-TILE_DIM/2)), TILE_DIM/2, TILE_DIM/2, x, y, TILE_DIM/2, TILE_DIM/2);
+				ctx.drawImage(img, this.drawx + (!e?0:(TILE_DIM)), this.drawy + (n?0:(-TILE_DIM/2)), TILE_DIM/2, TILE_DIM/2, x, y, TILE_DIM/2, TILE_DIM/2);
 				//southwest
-				ctx.drawImage(img, this.drawx + (e?0:(TILE_DIM)), this.drawy + (s?0:(TILE_DIM)), TILE_DIM/2, TILE_DIM/2, x, y, TILE_DIM/2, TILE_DIM/2);
+				ctx.drawImage(img, this.drawx + (!e?0:(TILE_DIM)), this.drawy + (s?0:(TILE_DIM)), TILE_DIM/2, TILE_DIM/2, x, y, TILE_DIM/2, TILE_DIM/2);
 			}
 			else{
 				ctx.drawImage(img,this.drawx,this.drawy,TILE_DIM,TILE_DIM,x,y, TILE_DIM, TILE_DIM);
