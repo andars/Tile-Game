afaca6de318cdf8359ed2eba6c990a3541fceaf5
andars <afoote97@gmail.com>
2013-05-06 18:29:25 -0600

    Update tile.js

--- tile.js
+++ tile.js
@@ -9,10 +9,11 @@ Tile.prototype = {
 	draw: function(ctx,img,x,y,level, tx, ty){
 		
 		if (this.split){
-			var n = !level.getTile(tx, ty-1).id == this.id;
-			var s = !level.getTile(tx, ty+1).id == this.id;
-			var w = !level.getTile(tx-1, ty).id == this.id;
-			var e = !level.getTile(tx+1, ty).id == this.id;
+			//true means not the same tile
+			var n = !level.getTile(tx, ty-1) === this;
+			var s = !level.getTile(tx, ty+1) === this;
+			var w = !level.getTile(tx-1, ty) === this;
+			var e = !level.getTile(tx+1, ty) === this;
 			if (n||s||w||e){
 				//northwest
 				ctx.drawImage(img, this.drawx + w?0:(-TILE_DIM/2), this.drawy + n?0:(-TILE_DIM/2), TILE_DIM/2,TILE_DIM/2,x,y,TILE_DIM/2,TILE_DIM/2);
